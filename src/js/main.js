2a6d720ce046baad5dfd779d818b8eb7dc052753
ricozhuthegreat <33135503+ricozhuthegreat@users.noreply.github.com>
2019-07-13 23:22:21 -0400

    Added Navbar in index.html
     - Added navbar and updated corresponding js and css

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,11 @@
 window.onscroll = function() {scrollNavbar()};
 
 function scrollNavbar () {
-  
+  if (window.scrollY > 5) {
+    let element = document.getElementById("mainNav");
+    element.classList.add("navbar-scroll");
+  } else {
+    let element = document.getElementById("mainNav");
+    element.classList.remove("navbar-scroll");
+  }
 }
